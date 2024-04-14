3687e16a8480f566f57f093d249c3b8ecbf48249
Soilenyis <soilenyis@gmail.com>
2024-04-14 16:00:02 -0300

    arrumar header e footer para telsa menores

--- src/pages/Compras/index.js
+++ src/pages/Compras/index.js
@@ -1,10 +1,14 @@
 
+import Banner from "../../components/Banner";
 import CompraResumo from "../../components/CompraResumo";
+import Rodape from "../../components/Rodape";
 
 const Compras = () => {
     return (
         <div>
+            <Banner />
             <CompraResumo />
+            <Rodape />
         </div>
     )
 }
